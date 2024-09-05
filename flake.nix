7d65acb13ebb4f3fe2071897ed9775fe7ba8643a
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-05 17:37:06 +0200

    flakify

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gurobi
+          ];
+          GUROBI_HOME = pkgs.gurobi.outPath;
+        };
+      }
+    );
+}
