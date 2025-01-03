1741c559a232824d01e17504ada0ccdd0a5656e6
Ughur Alakbarov <ugur.alekperov@gmail.com>
2025-01-03 16:29:30 +0100

    feat(ffi): take `*mut feasobj` in `GRBfeasrelax`
    From reference:[^1]
    "When minrelax=1, this returns the objective value for the minimum cost
    relaxation."
    
    So this modifies `feasobj`
    
    [^1]:
    https://docs.gurobi.com/projects/optimizer/en/11.0/reference/c/solving.html#c.GRBfeasrelax

--- grb-sys2/src/lib.rs
+++ grb-sys2/src/lib.rs
@@ -204,7 +204,7 @@ extern "C" {
         lbpen: *const c_double,
         ubpen: *const c_double,
         rhspen: *const c_double,
-        feasobjP: *const c_double,
+        feasobjP: *mut c_double,
     ) -> c_int;
 
     pub fn GRBfixmodel(model: *mut GRBmodel, new_model: *mut *mut GRBmodel) -> c_int;
